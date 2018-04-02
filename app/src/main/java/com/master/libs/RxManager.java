dd4e18ce30ac4e0ab7399540a36cd609c7bde87b
公司 <951827860@qq.com>
2018-04-02 14:03:41 +0800

    xxxx

---
+++ app/src/main/java/com/master/libs/RxManager.java
@@ -0,0 +1,23 @@
+package com.master.libs;
+
+import com.master.rxlib.Rx.Net.RetrofitHttpManger;
+
+/**
+ * Created by 不听话的好孩子 on 2018/4/2.
+ */
+
+public class RxManager {
+
+    public static <T> T create(Class<T> service) {
+        return SingleHolder.manger.get().create(service);
+    }
+
+    public static RetrofitHttpManger get() {
+        return SingleHolder.manger;
+    }
+
+    private static class SingleHolder {
+        //TODO
+        private static RetrofitHttpManger manger = new RetrofitHttpManger.Builder().Builder();
+    }
+}
--- rxlib/src/main/java/com/master/rxlib/Rx/Net/RetrofitHttpManger.java
+++ rxlib/src/main/java/com/master/rxlib/Rx/Net/RetrofitHttpManger.java
@@ -8,6 +8,7 @@ import com.master.rxlib.BuildConfig;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
@@ -50,16 +51,21 @@ public class RetrofitHttpManger {
         }
         return this;
     }
+
     public Retrofit get() {
         return mRetrofit;
     }
+
     public static class Builder {
         private int connectOut = 12;
         private int readOut = 12;
-        private String baseUrl=BuildConfig.defaultUrl;
+        private String baseUrl;
         private Retrofit mRetrofit;
+        private boolean showlog = true;
+        OkHttpClient httpclient;
         private File DownloadFILE = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
         private Map<String, String> headers = new LinkedHashMap<>();
+        private HttpsCerUtil.SSLParams sslParams;
 
         public Builder setTimeout(int connectOut, int readOut) {
             this.connectOut = connectOut;
@@ -77,50 +83,71 @@ public class RetrofitHttpManger {
             return this;
         }
 
+        public Builder setShowlog(boolean showlog) {
+            this.showlog = showlog;
+            return this;
+        }
+
         public Builder addHeaders(String key, String value) {
             headers.put(key, value);
             return this;
         }
 
+        public Builder addCert(InputStream[] certificates, InputStream bksFile, String password) {
+            sslParams = HttpsCerUtil.getSslSocketFactory(certificates, bksFile, password);
+            return this;
+        }
+
+        public Builder setOkhttpClient(OkHttpClient httpclient) {
+            this.httpclient = httpclient;
+            return this;
+        }
+
         public RetrofitHttpManger Builder() {
-            OkHttpClient httpclient = new OkHttpClient.Builder()
-                    .connectTimeout(connectOut, TimeUnit.SECONDS)
-                    .readTimeout(readOut, TimeUnit.SECONDS)
-                    .addInterceptor(new HttpLoggingInterceptor(new HttpLoggingInterceptor.Logger() {
+            if (httpclient == null) {
+                OkHttpClient.Builder builder = new OkHttpClient.Builder();
+                if (showlog) {
+                    builder.addInterceptor(new HttpLoggingInterceptor(new HttpLoggingInterceptor.Logger() {
                         @Override
                         public void log(String message) {
                             Log.i("HttpLoggingInterceptor", message);
                         }
-                    }).setLevel(BuildConfig.showlog ? HttpLoggingInterceptor.Level.BODY : HttpLoggingInterceptor.Level.NONE))
+                    }).setLevel(HttpLoggingInterceptor.Level.BODY));
+                }
+                builder.connectTimeout(connectOut, TimeUnit.SECONDS)
+                        .readTimeout(readOut, TimeUnit.SECONDS)
+                        // 添加公共参数拦截器
+                        // 添加通用的Header
+                        .addInterceptor(new Interceptor() {
+                            @Override
+                            public Response intercept(Chain chain) throws IOException {
+                                Request.Builder builder = chain.request().newBuilder();
+                                for (Map.Entry<String, String> entry : headers.entrySet()) {
+                                    builder.addHeader(entry.getKey(), entry.getValue());
+                                }
 
-                    // 添加公共参数拦截器
-                    // 添加通用的Header
-                    .addInterceptor(new Interceptor() {
-                        @Override
-                        public Response intercept(Chain chain) throws IOException {
-                            Request.Builder builder = chain.request().newBuilder();
-                            for (Map.Entry<String, String> entry : headers.entrySet()) {
-                                builder.addHeader(entry.getKey(), entry.getValue());
+                                return chain.proceed(builder.build());
                             }
-
-                            return chain.proceed(builder.build());
-                        }
-                    })
-                    .addInterceptor(new Interceptor() {
-                        @Override
-                        public Response intercept(Chain chain) throws IOException {
-                            Response originalResponse = chain.proceed(chain.request());
-                            String url = originalResponse.request().url().url().toString();
-                            if (progrssUrls.containsKey(url)) {
-                                return originalResponse.newBuilder()
-                                        .body(new ProgressDownloadBody(originalResponse.body(), progrssUrls.get(url), url))
-                                        .build();
-                            } else {
-                                return originalResponse;
+                        })
+                        .addInterceptor(new Interceptor() {
+                            @Override
+                            public Response intercept(Chain chain) throws IOException {
+                                Response originalResponse = chain.proceed(chain.request());
+                                String url = originalResponse.request().url().url().toString();
+                                if (progrssUrls.containsKey(url)) {
+                                    return originalResponse.newBuilder()
+                                            .body(new ProgressDownloadBody(originalResponse.body(), progrssUrls.get(url), url))
+                                            .build();
+                                } else {
+                                    return originalResponse;
+                                }
                             }
-                        }
-                    })
-                    .build();
+                        });
+                if (sslParams != null) {
+                    builder.sslSocketFactory(sslParams.sSLSocketFactory, sslParams.trustManager);
+                }
+                httpclient = builder.build();
+            }
             RetrofitHttpManger retrofitHttpManger = new RetrofitHttpManger();
             mRetrofit = new Retrofit.Builder()
                     .client(httpclient)
