34661b0cbc7c7630301964882da9372d5acf5d3d
公司 <951827860@qq.com>
2018-04-02 17:21:43 +0800

    自定义UI 线程和子线程扩展

---
+++ app/src/main/java/com/master/libs/AsyncExtends.kt
@@ -0,0 +1,40 @@
+package com.master.libs
+
+import android.os.Handler
+import android.os.Looper
+
+/**
+ * Created by 不听话的好孩子 on 2018/4/2.
+ */
+val handler = Handler(Looper.getMainLooper())
+var list = mutableListOf<Thread>()
+
+fun Any?.UI(block: () -> Unit) {
+    if (Thread.currentThread() == Looper.getMainLooper().thread) {
+        block()
+    } else {
+        handler.post(block)
+    }
+}
+
+fun removeAllCallBack() {
+    handler.removeCallbacksAndMessages(null)
+    for (thread in list) {
+        try {
+            thread.interrupt()
+        } catch (e: Exception) {
+        }
+    }
+    list.clear()
+}
+
+fun Any?.Async(block: () -> Unit, delay: Int = 0) {
+    if (Thread.currentThread() != Looper.getMainLooper().thread) {
+        block()
+    } else {
+        val thread = Thread(block)
+        list.add(thread)
+        thread.start()
+    }
+
+}
---
+++ app/src/main/java/com/master/libs/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.master.libs
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+
+import java.io.File
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val tv = findViewById<TextView>(R.id.tv)
+        Async {
+            NetUtils.download("http://sw.bos.baidu.com/sw-search-sp/software/d4e97ccd4bd9f/jdk-8u144-windows-i586_8.0.1440.1.exe", File(filesDir,"123.exe"),object :NetUtils.ProgressListener{
+                override fun call(current: Long, total: Long, speed: Int, index: String) {
+                    UI {
+                        tv.text = (String.format("%.2f", speed / 1024f) + "Mb/s" + "文件名: " + index + "----" + current * 100 / total + "%")
+                    }
+
+                }
+            })
+        }
+    }
+
+    fun go(v: View) {
+        startActivity(Intent(this, this.javaClass))
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        removeAllCallBack()
+    }
+}
