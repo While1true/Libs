d47285b6aa0ad76ced85df476f88517d4c20bbb0
公司 <951827860@qq.com>
2018-04-02 18:03:02 +0800

    自定义UI 线程和子线程扩展

--- app/src/main/java/com/master/libs/AsyncExtends.kt
+++ app/src/main/java/com/master/libs/AsyncExtends.kt
@@ -1,5 +1,7 @@
 package com.master.libs
 
+import android.arch.lifecycle.LifecycleObserver
+import android.arch.lifecycle.LifecycleOwner
 import android.os.Handler
 import android.os.Looper
 
@@ -7,34 +9,64 @@ import android.os.Looper
  * Created by 不听话的好孩子 on 2018/4/2.
  */
 val handler = Handler(Looper.getMainLooper())
-var list = mutableListOf<Thread>()
+var threads = linkedMapOf<Any, ArrayList<Thread>>()
+var runs = linkedMapOf<Any, ArrayList<Any>>()
 
-fun Any?.UI(block: () -> Unit) {
+fun AsyncIn.UI(block: () -> Unit) {
     if (Thread.currentThread() == Looper.getMainLooper().thread) {
         block()
     } else {
         handler.post(block)
     }
-}
-
-fun removeAllCallBack() {
-    handler.removeCallbacksAndMessages(null)
-    for (thread in list) {
-        try {
-            thread.interrupt()
-        } catch (e: Exception) {
-        }
+    var list = runs[hashCode()]
+    if (list == null) {
+        list = arrayListOf()
+        list.add(block)
+        runs[hashCode()] = list
+    } else {
+        list.add(block)
     }
-    list.clear()
 }
 
-fun Any?.Async(block: () -> Unit) {
+fun AsyncIn.removeAllCallBack() {
+    val arrayList = runs[hashCode()]
+    if (arrayList != null) {
+        for (a in arrayList) {
+            handler.removeCallbacks { a }
+        }
+        arrayList.clear()
+    }
+    var listThreads = threads[hashCode()]
+    if (listThreads != null) {
+        for (thread in listThreads) {
+            try {
+                thread.interrupt()
+            } catch (e: Exception) {
+            }
+        }
+        listThreads.clear()
+    }
+
+}
+
+fun AsyncIn.Async(block: () -> Unit) {
     if (Thread.currentThread() != Looper.getMainLooper().thread) {
         block()
     } else {
         val thread = Thread(block)
-        list.add(thread)
+        var threadx = threads[hashCode()]
+        if (threadx == null) {
+            threadx = arrayListOf()
+            threadx.add(thread)
+            threads[hashCode()] = threadx
+        } else {
+            threadx.add(thread)
+        }
         thread.start()
     }
+}
 
+interface AsyncIn {
+    fun onDestroy()
+//    fun getTraget(): Any
 }
--- app/src/main/java/com/master/libs/MainActivity.kt
+++ app/src/main/java/com/master/libs/MainActivity.kt
@@ -8,7 +8,7 @@ import android.widget.TextView
 
 import java.io.File
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),AsyncIn{
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
